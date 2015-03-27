5d3f7ebac35297f831359adfb9a957cfe1fb3001
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2015-03-27 17:08:11 +0200

    Update

---
+++ platforms/android/ant-build/PandaLauncher-debug-unaligned.apk.d
@@ -0,0 +1,9 @@
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/ant-build/PandaLauncher-debug-unaligned.apk : \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/ant-build/PandaLauncher.ap_ \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/ant-build/classes.dex \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/CordovaLib/xwalk_core_library/ant-build/classes.jar \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/CordovaLib/xwalk_core_library/libs/xwalk_core_library_java_app_part.jar \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/CordovaLib/xwalk_core_library/libs/xwalk_core_library_java_library_part.jar \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/CordovaLib/xwalk_core_library/libs/armeabi-v7a/libxwalkcore.so \
+/Users/eemelikelokorpi/Sites/panda.js-launcher/platforms/android/CordovaLib/xwalk_core_library/libs/x86/libxwalkcore.so \
---
+++ platforms/wp8/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/nl.x-services.plugins.insomnia/www/Insomnia.js",
+        "id": "nl.x-services.plugins.insomnia.Insomnia",
+        "clobbers": [
+            "window.plugins.insomnia"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "nl.x-services.plugins.insomnia": "4.0.1",
+    "org.apache.cordova.device": "0.3.0"
+}
+// BOTTOM OF METADATA
+});
